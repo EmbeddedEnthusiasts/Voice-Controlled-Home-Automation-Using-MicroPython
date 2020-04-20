58ffd0cf0a556ada1d2f8b32fe787f998288d272
EmbeddedEnthusiasts <63961487+EmbeddedEnthusiasts@users.noreply.github.com>
2020-04-20 21:25:36 +0530

    Update voice_controlled_home_automation_server.py

--- voice_controlled_home_automation_server.py
+++ voice_controlled_home_automation_server.py
@@ -12,18 +12,14 @@ print("connected to "+addr[0]+" and port= "+str(addr[1]))
 s1=sr.Recognizer()
 
 while True:
-     with sr.Microphone(device_index=2) as source:
+     with sr.Microphone() as source:
          s1.adjust_for_ambient_noise(source)
          voice=s1.listen(source)
      try:
         command=s1.recognize_google(voice)
-        if command=="thank you":
-            conn.send(str.encode(command))
-            break
-        else:
-            conn.send(str.encode(command))
-            clresp=str(conn.recv(1024),"utf-8")
-            print(clresp+"\n")
+        conn.send(str.encode(command))
+        clresp=str(conn.recv(1024),"utf-8")
+        print(clresp+"\n")
         del voice
      except:
         continue
