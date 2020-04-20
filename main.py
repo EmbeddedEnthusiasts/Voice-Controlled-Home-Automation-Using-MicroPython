d44e5bd3cc8e3a2a14fe23681adc175e1cf5a932
EmbeddedEnthusiasts <63961487+EmbeddedEnthusiasts@users.noreply.github.com>
2020-04-20 20:32:08 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,58 @@
+import socket
+from machine import Pin
+import network 
+
+pin=Pin(4,Pin.OUT)
+pin1=Pin(0,Pin.OUT)  
+pin.on()
+pin1.on()
+mes1="fan turned on"
+mes2="fan turned off"
+mes3="light turned on"
+mes4="light turned off"
+mes5="invalid command"
+def do_connect():
+    import network
+    sta_if = network.WLAN(network.STA_IF)
+    if not sta_if.isconnected():
+        print('connecting to network...')
+        sta_if.active(True)
+        sta_if.connect('Aavishkar', 'rohit$276')
+        while not sta_if.isconnected():
+            pass
+    print('network config:', sta_if.ifconfig())
+
+   
+do_connect()
+host="192.168.0.109"
+port=9999
+
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.connect((host,port))
+
+while True:
+    data=str(s.recv(1024),"utf-8")
+    if "on fan" in data:
+      s.send(str.encode(mes1))
+      pin.off()
+      print(data)
+      
+    elif "off fan" in data:
+        s.send(str.encode(mes2))
+        pin.on()
+        print(data)
+    elif "on light" in data:
+        s.send(str.encode(mes3))
+        pin1.off()
+        print(data)
+    elif "off light" in data:
+        s.send(str.encode(mes4))
+        pin1.on()
+        print(data)
+    else:
+        print("host entered invalid command")
+        s.send(str.encode(mes5))
+        continue
+
+
+
---
+++ voice_controlled_home_automation_server.py
@@ -0,0 +1,32 @@
+import speech_recognition as sr 
+import socket 
+
+host="put_you_computer_address_here"
+port=9999
+s=socket.socket()
+s.bind((host,port))
+s.listen(5)
+conn,addr=s.accept()
+print("connected to "+addr[0]+" and port= "+str(addr[1]))
+
+s1=sr.Recognizer()
+
+while True:
+     with sr.Microphone(device_index=2) as source:
+         s1.adjust_for_ambient_noise(source)
+         voice=s1.listen(source)
+     try:
+        command=s1.recognize_google(voice)
+        if command=="thank you":
+            conn.send(str.encode(command))
+            break
+        else:
+            conn.send(str.encode(command))
+            clresp=str(conn.recv(1024),"utf-8")
+            print(clresp+"\n")
+        del voice
+     except:
+        continue
+
+conn.close()
+print("connection ended")
